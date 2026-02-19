8b363534d0efe98015e98fa14f6544cdd4679385
ANIKET SRIVASTAVA <aniket08official@gmail.com>
2026-02-19 12:25:36 +0530

    string started !

---
+++ strings/easy/reverse.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str = "abcdefgh";
+    reverse(str.begin(),str.begin()+3);
+    cout<<"rev -> "<<str<<endl;
+    return 0;
+}
